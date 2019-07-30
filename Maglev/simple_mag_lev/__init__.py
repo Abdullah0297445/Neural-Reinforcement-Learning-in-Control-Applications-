5d01644c692b374635b3ec27e76234731a09d752
abdullah0297445 <abdullah.ajmal.7@hotmail.com>
2019-07-30 16:37:07 +0500

    change folders layout

--- Maglev/simple_mag_lev/__init__.py
+++ Maglev/simple_mag_lev/__init__.py
@@ -1,1 +1,9 @@
-from gym_simple_mag_lev.envs.maglevEnv import MagLevEnv
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='simple_mag_lev-v0',
+    entry_point='gym_simple_mag_lev.envs:MagLevEnv',
+)
